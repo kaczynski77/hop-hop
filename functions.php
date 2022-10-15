c3f2fe6a0a13dd06be0b390470ba4c8e7057de8f
Gennady <pospelov@Gennadys-MacBook-Pro.local>
2022-10-16 00:28:46 +0300

    fonts + modal

--- functions.php
+++ functions.php
@@ -5,8 +5,7 @@ function customtheme_add_woocommerce_support()
 }
 add_action('after_setup_theme', 'customtheme_add_woocommerce_support');
 
-add_action( 'wp_enqueue_scripts', 'hophop_styles' );
-add_action( 'wp_enqueue_scripts', 'hophop_scripts' );
+
 
 function hophop_styles() {
 	wp_enqueue_style( 'style', get_template_directory_uri() . '/css/style.css', array(),  filemtime( get_stylesheet_directory_uri() . '/css/style.css'));
@@ -18,6 +17,16 @@ function hophop_scripts() {
 	wp_enqueue_script( 'mui', get_template_directory_uri() . '/js/mui.min.js', array(), );
      wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array(),  filemtime( get_stylesheet_directory_uri() . '/js/main.js') );
 }
+
+function hophop_fonts() {
+    wp_enqueue_style( 'futurabookc', get_template_directory_uri() . '/fonts/futurabookc.otf', false );
+    wp_enqueue_style( 'FUTURADEMIC-ITALIC', get_template_directory_uri() . '/fonts/FUTURADEMIC-ITALIC.TTF', false );
+    wp_enqueue_style( 'FUTURADEMIC', get_template_directory_uri() . '/fonts/FUTURADEMIC.TTF', false );
+}
+
+add_action( 'wp_enqueue_scripts', 'hophop_fonts' );
+add_action( 'wp_enqueue_scripts', 'hophop_styles' );
+add_action( 'wp_enqueue_scripts', 'hophop_scripts' );
 
 /* add_action('widgets_init', 'my_register_sidebars');
 function my_register_sidebars()
--- header.php
+++ header.php
@@ -20,3 +20,89 @@
                     } ?> -->
 
         </div>
+        <section id="checkout">
+        <div class="mui-container">
+            <span class="close">X</span>
+            <form class="mui-form">
+                <legend>Детали заказа</legend>
+                <div class="fields">
+                    <div class="row">
+                        <div class="mui-textfield">
+                            <input type="text" placeholder="Input 1">
+                            <label>Имя</label>
+                        </div>
+
+                        <div class="mui-textfield">
+                            <input type="text" placeholder="Input 2">
+                            <label>Фамилия</label>
+                        </div>
+                    </div>
+                    <div class="mui-select">
+                        <select>
+                            <option>ЛНР/ДНР/РФ</option>
+                            <option>ЛНР</option>
+                            <option>ДНР</option>
+                            <option>РФ</option>
+                        </select>
+                        <label>Страна/регион</label>
+                    </div>
+
+                    <div class="mui-textfield">
+                        <input type="text" placeholder="Input 2">
+                        <label>Адрес</label>
+                    </div>
+                    <div class="mui-textfield">
+                        <input type="text" placeholder="Input 2">
+                        <label>Населённый пункт</label>
+                    </div>
+                    <div class="mui-textfield">
+                        <input type="text" placeholder="Input 2">
+                        <label>Область</label>
+                    </div>
+                    <div class="mui-textfield">
+                        <input type="text" placeholder="Input 2">
+                        <label>Индекс</label>
+                    </div>
+                    <div class="mui-textfield">
+                        <input type="text" placeholder="Input 2">
+                        <label>Телефон</label>
+                    </div>
+                    <div class="mui-textfield">
+                        <input type="text" placeholder="Input 2">
+                        <label>Email</label>
+                    </div>
+                    <div class="mui-textfield">
+                        <textarea placeholder="Textarea"></textarea>
+                        <label>Коменатрии к заказу</label>
+                    </div>
+                    <div class="cart_content">
+                        <h3>Ваш заказ</h3>
+                        <ul>
+                            <li>
+                                <span>Пончо / размер L / цвет чёрный</span>
+                                <span>xl</span>
+                                <span>5000</span>
+                            </li>
+                            <li>
+                                <span>Пончо / размер L / цвет чёрный</span>
+                                <span>xl</span>
+                                <span>5000</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="price_total">
+                        <h3>ИТОГ:</h3><span>10000</span>
+                    </div>
+                    <div class="delivery">
+                        <h3>Доставка</h3><span>Доставка согласно тариуф "Транспортной
+                            компании/почты" и в тоговою
+                            стоимость на сайте не включена</span>
+                    </div>
+                    <button type="submit" class="mui-btn mui-btn--raised">Заказать</button>
+                </div>
+
+            </form>
+        </div>
+        </div>
+
+    </section>
