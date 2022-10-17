5462b392d088d97d45ba1b896b66f0e46d778ff8
Gennady <pospelov@Gennadys-MBP.lan>
2022-10-17 19:06:37 +0300

    ajax checkout tweaks

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
                 <span><a href='<?php echo get_site_url()?>'>Хоп-хоп</a></span>
                 <span>Мужчина</span>
                 <span>Женщина</span>
-                <span><button onclick='customCheckout(event)'>test</button></test>
+                <span><button onclick='customCheckout(event); displayCheckout();'>test</button></test>
                     <div class="xoo-wsc-cart-trigger">Trigger Side Cart</div>
                     <!-- <?php $ajax_cart_en = 'yes' === get_option('woocommerce_enable_ajax_add_to_cart');
                     if ($ajax_cart_en) {
