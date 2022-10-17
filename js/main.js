d3dd4b10c08424b346ac691b0a77f1150050d60e
Gennady <pospelov@Gennadys-MBP.lan>
2022-10-17 18:41:17 +0300

    ajax checkout tweaks

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ const sidebarControl = () => {
 }
 
 function customCheckout(event) {
-    displayCheckout();
+    
     var wp_ajax_url = "/wp-admin/admin-ajax.php";
     var data = {
         action: 'getCheckoutPageContent',
@@ -52,6 +52,7 @@ function customCheckout(event) {
     };
 
     jQuery.post(wp_ajax_url, data, function (content) {
+        displayCheckout();
         jQuery("#checkOutPageContent").html(content);
   });
      
